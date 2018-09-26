a033925cdd398765ebb81faaa7e4fa27849208a3
Billy Wolfington <wolfington98@gmail.com>
2018-09-25 19:59:15 -0700

    Initial commit of project files

---
+++ NServiceBus.HostBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NServiceBus.HostBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
