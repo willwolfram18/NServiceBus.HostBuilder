259bec34fb601e137e46437f250530b5b11f7510
Billy Wolfington <wolfington98@gmail.com>
2018-09-25 20:44:22 -0700

    Program implemented

---
+++ NServiceBus.HostBuilder/NServiceBusHostedService.cs
@@ -0,0 +1,31 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace NServiceBus.HostBuilder
+{
+    public class NServiceBusHostedService : IHostedService
+    {
+        private IEndpointInstance _endpoint;
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var endpointConfig = new EndpointConfiguration("ExampleEndpoint");
+
+            endpointConfig.UsePersistence<LearningPersistence>();
+            endpointConfig.UseTransport<LearningTransport>();
+
+            _endpoint = await Endpoint.Start(endpointConfig);
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            if (_endpoint == null)
+            {
+                return;
+            }
+
+            await _endpoint.Stop();
+        }
+    }
+}
--- NServiceBus.HostBuilder/Program.cs
+++ NServiceBus.HostBuilder/Program.cs
@@ -1,13 +1,44 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace NServiceBus.HostBuilder
 {
     class Program
     {
+        private static string Directory => AppDomain.CurrentDomain.BaseDirectory;
+
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var hostBuilder = new Microsoft.Extensions.Hosting.HostBuilder()
+                                .ConfigureHostConfiguration(config =>
+                                {
+                                    config.SetBasePath(Directory)
+                                        .AddEnvironmentVariables(prefix: "ASPNETCORE_");
+                                })
+                                .ConfigureAppConfiguration(config =>
+                                {
+                                    config.SetBasePath(Directory)
+                                        .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                                        .AddJsonFile("appsettings.json");
+                                })
+                                .ConfigureServices(services =>
+                                {
+                                    services.AddHostedService<NServiceBusHostedService>();
+                                });
+
+            if (Debugger.IsAttached || args.Contains("--console"))
+            {
+                await hostBuilder.RunConsoleAsync();
+            }
+            else
+            {
+                await hostBuilder.RunAsWindowsServiceAsync();
+            }
         }
     }
 }
