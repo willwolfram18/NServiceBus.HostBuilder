5aaa348c0f729accb59201140e12483e0477a991
Billy Wolfington <wolfington98@gmail.com>
2018-09-25 20:20:46 -0700

    Windows Service host lifetime

---
+++ NServiceBus.HostBuilder/WindowsServiceLifetime.cs
@@ -0,0 +1,65 @@
+using System;
+using System.ServiceProcess;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace NServiceBus.HostBuilder
+{
+    internal class WindowsServiceLifetime : ServiceBase, IHostLifetime
+    {
+        private readonly TaskCompletionSource<object> _taskForLifetime = new TaskCompletionSource<object>();
+
+        private readonly IApplicationLifetime _appLifetime;
+
+        public WindowsServiceLifetime(IApplicationLifetime appLifetime)
+        {
+            _appLifetime = appLifetime ?? throw new ArgumentNullException(nameof(appLifetime));
+        }
+
+        public Task WaitForStartAsync(CancellationToken cancellationToken)
+        {
+            cancellationToken.Register(() => _taskForLifetime.TrySetCanceled());
+
+            _appLifetime.ApplicationStopping.Register(Stop);
+
+            new Thread(StartWindowsService).Start();
+
+            return _taskForLifetime.Task;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Stop();
+            return Task.CompletedTask;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _taskForLifetime.TrySetResult(null);
+
+            base.OnStart(args);
+        }
+
+        protected override void OnStop()
+        {
+            _appLifetime.StopApplication();
+
+            base.OnStop();
+        }
+
+        private void StartWindowsService()
+        {
+            try
+            {
+                Run(this);
+
+                _taskForLifetime.TrySetException(new InvalidOperationException("Windows Services stopped without starting."));
+            }
+            catch (Exception)
+            {
+                _taskForLifetime.TrySetException(e);
+            }
+        }
+    }
+}
